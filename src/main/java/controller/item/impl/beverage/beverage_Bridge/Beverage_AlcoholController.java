351435842d9e96a3383d617fbc92fd9b3926038a
ESPOIR <espoirditekemena@gmail.com>
2019-09-25 22:16:37 -0700

    dealing with item controller

--- src/main/java/controller/item/impl/beverage/beverage_Bridge/Beverage_AlcoholController.java
+++ src/main/java/controller/item/impl/beverage/beverage_Bridge/Beverage_AlcoholController.java
@@ -25,7 +25,7 @@ public class Beverage_AlcoholController implements ControllerInt<Beverage_Alcoho
     @PostMapping("/create")
     @Override
     public Beverage_Alcohol create(@RequestBody Beverage_Alcohol beverage_alcohol) {
-        String itemId=itemService.read();
+        //String itemId=itemService.read();
         String beverageId=beverage_alcohol.getItem_id();
         return beverage_alcoholServ.create(beverage_alcohol);
     }
