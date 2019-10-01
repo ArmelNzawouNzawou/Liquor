222c4bc52c1184e62d289eaffa19bea5d9d8718c
ESPOIR <espoirditekemena@gmail.com>
2019-10-01 14:21:00 -0700

    done with selling

--- src/main/java/company/com/controller/customerDelivery/CustomerDeveliveryController.java
+++ src/main/java/company/com/controller/customerDelivery/CustomerDeveliveryController.java
@@ -2,17 +2,17 @@ package company.com.controller.customerDelivery;
 
 import company.com.controller.ControllerInt;
 import company.com.domain.customerDelivery.CustomerDelivery;
+import company.com.domain.item.impl.beverage.alcohol.impl.Beer;
 import company.com.domain.orderBuilder.Orders;
 import company.com.domain.orderLine.OrderLine;
+import company.com.factory.domain.customerDelivery.CustomerDeliveryFactory;
 import company.com.factory.domain.orderFactory.OrderFacto;
 import company.com.factory.domain.orderLine.OrderLineFactory;
+import company.com.service.itemTrensaction.ItemStockService;
 import company.com.service.orderLine.impl.OrderLineService;
 import company.com.service.orderServices.impl.OrderServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -23,18 +23,40 @@ public class CustomerDeveliveryController implements ControllerInt<CustomerDeliv
     OrderServices orderServices;
 @Autowired
     OrderLineService orderLineService;
+@Autowired
+    ItemStockService itemStockService;
 
     @PostMapping("/create")
     @Override
     public CustomerDelivery create(@RequestBody CustomerDelivery cus) {
         Orders orders= OrderFacto.getOrders(cus.getCustomerNumber(),cus.getOrderTime(),cus.getComment());
+        orderServices.create(orders);
 
-
+        if(cus.getItemID()!=null){
+            for(int i=0;i<cus.getItemID().size();i++){
+            OrderLine orderLine= OrderLineFactory.getOrderLine(orders.getOrderNumeber(),cus.getItemID().get(i));
+            orderLineService.create(orderLine);
+            /**
+             * i need to sell the item
+             * means to reduce the quantity of the item;
+             */
+            itemStockService.sellItem(cus.getItemID().get(i),cus.getQuatity().get(i));
+            //orderServices.create(orders);
+            /**
+             * now we want to return a meaningful response to the customer.
+             */
+            CustomerDelivery cd= CustomerDeliveryFactory.getCustomerDelivery(orders.getOrderNumeber(),cus.getItemID(),cus.getCustomerNumber(),cus.getOrderTime(),cus.getComment(),cus.getQuatity());
+            return cd;
+        }}
         return null;
     }
 
+    @GetMapping("/read")
     @Override
-    public CustomerDelivery read(String id) {
+    public CustomerDelivery read(@RequestParam String id) {
+        while(orderLineService.read(id)!=null){
+
+        }
         return null;
     }
 
@@ -50,6 +72,15 @@ public class CustomerDeveliveryController implements ControllerInt<CustomerDeliv
 
     @Override
     public ArrayList<CustomerDelivery> readAll() {
+        return null;
+    }
+    public Object createItem(String itemType,String itemId){
+        switch (itemType){
+            case"alcohol":
+               // return new Beer();
+
+        }
+
         return null;
     }
 }
--- src/main/java/company/com/domain/customerDelivery/CustomerDelivery.java
+++ src/main/java/company/com/domain/customerDelivery/CustomerDelivery.java
@@ -4,12 +4,22 @@ import java.util.ArrayList;
 
 public class CustomerDelivery {
     private String OrderId;
+    private ArrayList<String> itemID;
     private String customerNumber;
     private String orderTime;
     private String comment;
-    //private String itemNumber
-
+    private ArrayList<Integer> quatity;
+    private String itemName;
+    private String itemSize;
     private CustomerDelivery() {
+    }
+
+    public ArrayList<Integer> getQuatity() {
+        return quatity;
+    }
+
+    public void setQuatity(ArrayList<Integer> quatity) {
+        this.quatity = quatity;
     }
 
     public String getOrderId() {
@@ -20,6 +30,13 @@ public class CustomerDelivery {
         OrderId = orderId;
     }
 
+    public ArrayList<String> getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(ArrayList<String> itemID) {
+        this.itemID = itemID;
+    }
 
     public String getCustomerNumber() {
         return customerNumber;
@@ -49,16 +66,22 @@ public class CustomerDelivery {
     public String toString() {
         return "CustomerDelivery{" +
                 "OrderId='" + OrderId + '\'' +
+                ", itemID=" + itemID +
                 ", customerNumber='" + customerNumber + '\'' +
                 ", orderTime='" + orderTime + '\'' +
                 ", comment='" + comment + '\'' +
+                ", quatity='" + quatity + '\'' +
                 '}';
     }
+
     public static class Builder{
         private String OrderId;
+        private ArrayList<String> itemID;
         private String customerNumber;
         private String orderTime;
         private String comment;
+        private ArrayList<Integer> quatity;
+
         public Builder(String customerNumber){
             this.customerNumber=customerNumber;
         }
@@ -74,11 +97,19 @@ public class CustomerDelivery {
             this.comment=comment;
             return this;
         }
-
+        public Builder buildItemId(ArrayList<String> itemID){
+            this.itemID=itemID;
+            return this;
+        }
+        public Builder buildQuantity(ArrayList<Integer> quatity){
+            this.quatity=quatity;
+            return this;
+        }
         public CustomerDelivery build(){
             CustomerDelivery cust=new CustomerDelivery();
             cust.comment=this.comment;
             cust.customerNumber=this.customerNumber;
+            cust.itemID=this.itemID;
             cust.OrderId=this.OrderId;
             cust.orderTime=this.orderTime;
             return cust;
